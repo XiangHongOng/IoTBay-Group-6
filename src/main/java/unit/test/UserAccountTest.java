04f06c2c891cc22d798475c2e133617eb597e49a
Rishan-Jadva <rishanjadva@gmail.com>
2024-05-17 10:27:29 +1000

    UserAccountTest 5 new tests

--- src/main/java/unit/test/UserAccountTest.java
+++ src/main/java/unit/test/UserAccountTest.java
@@ -4,7 +4,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.project.iotprojecttest.model.dao.UserDAO;
+import org.project.iotprojecttest.model.objects.Customer;
 import org.project.iotprojecttest.model.objects.User;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserAccountTest
 {
@@ -18,7 +21,7 @@ public class UserAccountTest
     @Test
     public void testCreateUser() {
         User user = new User();
-        user.setEmail("john@example.com");
+        user.setEmail("jim@example.com");
         user.setPassword("password");
         user.setPhone("1234567890");
 
@@ -29,10 +32,8 @@ public class UserAccountTest
 
         User createdUser = userDAO.getUserById(userId);
 
-        if (createdUser != null)
-        {
-            userDAO.deleteUser(userId);
-        }
+        userDAO.deleteUser(userId);
+
     }
 
     @Test
@@ -42,6 +43,59 @@ public class UserAccountTest
 
         Assert.assertNotNull(user);
         Assert.assertEquals(1, user.getUserId());
+    }
+
+    @Test
+    public void testGetNumberOfUsers(){
+        UserDAO userDAO = new UserDAO();
+        List<User> users = userDAO.getAllUsers();
+        Assert.assertEquals(25, users.size());
+    }
+
+    @Test
+    public void testVerifyUserExists(){
+        UserDAO userDAO = new UserDAO();
+        User user = new User();
+
+        user.setEmail("jerry@example.com");
+        user.setPassword("password");
+        user.setPhone("1234567890");
+
+        int userId = userDAO.createUser(user);
+
+        Assert.assertTrue(userDAO.verifyUser(user.getEmail(), user.getPassword()));
+
+        userDAO.deleteUser(userId);
+    }
+
+    @Test
+    public void testVerifyUserNotExists(){
+        Assert.assertFalse(userDAO.verifyUser("notanemail@example.com", "badPassword"));
+    }
+
+    @Test
+    public void testEmailDoesNotExist(){
+        UserDAO userDAO = new UserDAO();
+        User user = userDAO.getUserByEmail("abcdefg@gmail.com");
+        Assert.assertNull(user);
+    }
+
+    @Test
+    public void testEmailDoesExist(){
+        UserDAO userDAO = new UserDAO();
+        User user = new User();
+
+        user.setEmail("jerry@example.com");
+        user.setPassword("password");
+        user.setPhone("1234567890");
+
+        int userId = userDAO.createUser(user);
+
+        User userFound = userDAO.getUserByEmail("jerry@example.com");
+
+        Assert.assertNotNull(userFound);
+
+        userDAO.deleteUser(userId);
     }
 
     @Test
@@ -62,7 +116,7 @@ public class UserAccountTest
     public void testDeleteUser() {
         UserDAO userDAO = new UserDAO();
         User user = new User();
-        user.setEmail("john@example.com");
+        user.setEmail("jeremy@example.com");
         user.setPassword("password");
         user.setPhone("1234567890");
 
